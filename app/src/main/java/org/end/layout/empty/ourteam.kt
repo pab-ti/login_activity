b6dccee73d79e84738222ad10fc8cc2fe7129ab5
Mercphobia <droidsourcecode@outlook.com>
2022-11-11 21:42:04 +0700

    Frist Commit

---
+++ app/src/main/java/org/end/layout/empty/ourteam.kt
@@ -0,0 +1,11 @@
+package org.end.layout.empty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ourteam : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ourteam)
+    }
+}
